1c20d676c676968e4904960279e35e4cad60a36a
Jockytoky-prtcl <119138706+Jockytoky-prtcl@users.noreply.github.com>
2023-06-01 18:18:30 +0200

    Create main.rs

---
+++ src/main.rs
@@ -0,0 +1,16 @@
+extern crate wiringpi;
+
+use wiringpi::pin::Value::{High, Low};
+use std::{thread, time};
+fn main() {
+    println!("process has started");
+    let interval = time::Duration::from_millis(1000);
+    let wiringpi = wiringpi::setup();
+    let pin = wiringpi.output_pin(0);
+    loop {
+        pin.digital_write(High);
+        thread::sleep(interval);
+        pin.digital_write(Low);
+        thread::sleep(interval);
+    }
+}
